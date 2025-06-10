7826d95147255da62b67a62f25ba876410078bb2
pascales <pascales_kurniawan@linkaja.id>
2025-06-10 11:54:40 +0700

    add upload plugin

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -143,6 +143,7 @@ export interface User {
 export interface Media {
   id: string;
   alt: string;
+  _key?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -308,6 +309,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  _key?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { uploadthingStorage } from '@payloadcms/storage-uploadthing'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -37,5 +38,14 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    uploadthingStorage({
+      collections: {
+        media: true,
+      },
+      options: {
+        token: process.env.UPLOADTHING_TOKEN,
+        acl: 'public-read',
+      },
+    }),
   ],
 })
